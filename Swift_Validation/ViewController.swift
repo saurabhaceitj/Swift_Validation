862ebf4618653de95baaef5a3bd486ed80531d1d
Saurabh <saurabh.jain@aryavratinfotech.com>
2017-07-11 19:53:05 +0530

    First Commit

--- Swift_Validation/ViewController.swift
+++ Swift_Validation/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtFiled: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnDoneDidClicked(_ sender: UIButton) {
+        if (txtFiled.text?.isValidEmailId)! {
+            print("Valid Email")
+        }
+        
+        if (txtFiled.text?.isValidPassword)! {
+            print("Valid Password")
+        }
+        
+        if (txtFiled.text?.isValidUserName)! {
+            print("Valid User Phone")
+        }
+        
+        if (txtFiled.text?.isValidMobileNumber)! {
+            print("Valid Mobile Number")
+        }
+        
+        if (txtFiled.text?.isValidEmailAndNumber)! {
+            print("Valid Email and Number")
+        }
+
+    }
 
 }
 
